71d6c892b7dbf0670fdd239b56ecbed579996445
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-03-01 12:21:30 +0000

    Walkthrough part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-DaZgryg_ksAqVlgfXglcr00DhrM
+const API_KEY = "DaZgryg_ksAqVlgfXglcr00DhrM";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
